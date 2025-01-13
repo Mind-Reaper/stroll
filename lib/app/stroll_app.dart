15d9659761a3c08a6776a3c10dc24b6aa18633ca
Mind-Reaper <onadipedaniel@gmail.com>
2025-01-13 23:34:56 +0100

    Added Packages, Added Assets: Fonts, Icons, Images

---
+++ lib/app/stroll_app.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class StrollApp extends StatelessWidget {
+  const StrollApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
